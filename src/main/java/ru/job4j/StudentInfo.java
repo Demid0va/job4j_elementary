944073c672e6976352b424191feba095f1a773e6
polina <p.demid0va@yandex.ru>
2023-01-03 17:20:27 +0300

    Multiple

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Demidova Polina");
+        System.out.println("10.02.1997");
+    }
+}
