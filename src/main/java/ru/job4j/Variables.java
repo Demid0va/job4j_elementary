ce05377ede02143eeb8808e405c2243b0a765e6a
polina <p.demid0va@yandex.ru>
2023-01-07 19:12:26 +0300

    variables

--- src/main/java/ru/job4j/Variables.java
+++ src/main/java/ru/job4j/Variables.java
@@ -4,14 +4,16 @@ public class Variables {
     public static void main(String[] args) {
         byte age = 19;
         System.out.println(age);
-        boolean gender = false;
+        char gender = 1;
         System.out.println(gender);
-        short growth = 170;
+        float growth = 170;
         System.out.println(growth);
         int bricks = 24480;
         System.out.println(bricks);
         double bacteria = 1.05;
         System.out.println(bacteria);
+        int space = 149597870;
+        System.out.println(space);
         boolean permission = false;
         System.out.println(permission);
         int file = 60000202;
