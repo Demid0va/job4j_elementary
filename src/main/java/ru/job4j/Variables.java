c6d31a3f86a1ce84c34b87ca352427f07648d34d
polina <p.demid0va@yandex.ru>
2023-01-08 18:38:52 +0300

    Calculator

--- src/main/java/ru/job4j/Variables.java
+++ src/main/java/ru/job4j/Variables.java
@@ -4,15 +4,15 @@ public class Variables {
     public static void main(String[] args) {
         byte age = 19;
         System.out.println(age);
-        char gender = 1;
+        char gender = 'Ж';
         System.out.println(gender);
-        float growth = 170;
+        float growth = 1.7F;
         System.out.println(growth);
         int bricks = 24480;
         System.out.println(bricks);
         double bacteria = 1.05;
         System.out.println(bacteria);
-        int space = 149597870;
+        double space = 149597870;
         System.out.println(space);
         boolean permission = false;
         System.out.println(permission);
