b0321cc62fdfeef18401c662683cc98f26989314
polina <p.demid0va@yandex.ru>
2023-01-09 20:20:45 +0300

    Calculator

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,19 +1,11 @@
 package ru.job4j;
 public class Calculator {
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int four = 4;
-        int five = 5;
-        int six = 6;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
-        int sixDivTwo = six / two;
-        System.out.println(sixDivTwo);
-        int fiveMinusTwo = five - two;
-        System.out.println(fiveMinusTwo);
-        int fourTimeTwo = four * two;
-        System.out.println(fourTimeTwo);
-
+        int size = 10;
+        System.out.println(size);
+        size = 100;
+        System.out.println(size);
+        int result = size - 5;
+        System.out.println(result);
     }
 }
