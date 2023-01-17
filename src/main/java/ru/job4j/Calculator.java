44df92777ced65572e867742f9c6fa0428830bde
polina <p.demid0va@yandex.ru>
2023-01-17 16:44:25 +0300

    Calculator

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,13 +1,22 @@
-package ru.job4j;
+ackage ru.job4j.calculator;
+
 public class Calculator {
+
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
 
-    public static void main(String[] args) {
-        Calculator.plus(100, 500);
-        Calculator.plus(4, 2);
-        Calculator.plus(3, 5);
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
     }
+
+    public static void main(String[] args) {
+        plus(1, 2);
+        plus(10, 11);
+        minus(7, 3);
+        minus(2, 6);
+    }
+
 }
