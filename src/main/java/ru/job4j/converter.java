74d5d990a23bf3d571e9775b3006960869d1052f
polina <p.demid0va@yandex.ru>
2023-01-17 17:59:30 +0300

    Converter

--- src/main/java/ru/job4j/converter.java
+++ src/main/java/ru/job4j/converter.java
@@ -1,19 +1,25 @@
-package ru.job4j.converter;
+package ru.job4j;
 
-public class Converter {
+public class converter {
 
     public static float rubleToEuro(float value) {
         float rsl = value / 70;
         return rsl;
     }
+    public static float rubleToEuro2(float value) {
+        float rsl = value / 60;
+        return rsl;
+    }
 
     public static float rubleToDollar(float value) {
-        float rsl = -1; /* формула перевода рублей в доллары. */
+        float rsl = 1; 
         return rsl;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
+        float euro = converter.rubleToEuro(70);
+        float dollar = converter.rubleToDollar(60);
+        System.out.println("60 rubles are " + euro + " euro.");
+        System.out.println("70 rubles are " + dollar + " dollar.");
     }
 }
