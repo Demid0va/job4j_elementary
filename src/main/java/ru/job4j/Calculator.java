4935116b4780e5d2ba0e027c5da605e829ce31e4
polina <p.demid0va@yandex.ru>
2023-01-30 22:23:31 +0300

    Fit

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,12 +1,13 @@
 package ru.job4j;
+
 public class Calculator {
     public static double manWeight(short height) {
-        double rsl = (187 - 100) * 1.15;
+        double rsl = (height - 100) * 1.15;
         return rsl;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (170 - 110) * 1.15;
+        double rsl = (height - 110) * 1.15;
         return rsl;
     }
 
